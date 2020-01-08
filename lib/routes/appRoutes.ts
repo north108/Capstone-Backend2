5e1faccf0aa45261e79286f2cd28b69d322b31e8
Elizabeth Northrop <elizabethjnorthrop@gmail.com>
2020-01-07 17:04:57 -0800

    Finished more route skeletons.

--- lib/routes/appRoutes.ts
+++ lib/routes/appRoutes.ts
@@ -8,5 +8,39 @@ export class Routes {
         message: 'GET request successful!'
       })
     })
+
+    app.route('/users')
+    // get all users
+    .get((request: Request, response: Response) => {
+      response.status(200).send({
+        message: 'GET request successful!'
+      })
+    })
+    // create new user
+    .post((request: Request, response: Response) => {
+      response.status(200).send({
+        message: 'POST request successful!'
+      })
+    })
+
+    app.route('/users/:userId')
+    // show user
+    .get((request: Request, response: Response) => {
+      response.status(200).send({
+        message: 'GET request successful!'
+      })
+    })
+    // Update a user
+    .put((request: Request, response: Response) => {        
+      response.status(200).send({
+        message: 'PUT request successful!'
+      })
+    })
+    // Delete a user
+    .delete((request: Request, response: Response) => {
+      response.status(200).send({
+        message: 'DELETE request successful!'
+      })
+    })
   }
 }
