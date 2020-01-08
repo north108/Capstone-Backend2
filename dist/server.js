c260d709d5552992338ca98ee506a9cf6874f23c
Elizabeth Northrop <elizabethjnorthrop@gmail.com>
2020-01-08 13:57:12 -0800

    Fixed the port for the server.

--- dist/server.js
+++ dist/server.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const app_1 = require("./app");
-const PORT = 3200;
+const PORT = (process.env.PORT || 3200);
 app_1.default.listen(PORT, () => {
     console.log(`Express server is running on port ${PORT}.`);
 });
--- lib/server.ts
+++ lib/server.ts
@@ -1,6 +1,6 @@
 import app from "./app";
 
-const PORT = 3200
+const PORT = (process.env.PORT || 3200)
 
 app.listen(PORT, () => {
   console.log(`Express server is running on port ${PORT}.`);
